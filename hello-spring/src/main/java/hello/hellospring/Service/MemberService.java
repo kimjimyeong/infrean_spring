5ccc21f95b2358a60aaf5102f712ca6474207331
kimjimyeong <ehxhfl1589@gmail.com>
2023-03-27 20:55:17 +0900

    2023-03-27 21:00 commit

---
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -0,0 +1,42 @@
+package hello.hellospring.Service;
+
+import hello.hellospring.Domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     *
+     */
+    public Long join(Member member){
+        // 같은 중복 이름은 가입 불가능
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
